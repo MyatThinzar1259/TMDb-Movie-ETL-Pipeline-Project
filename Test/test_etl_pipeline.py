4ac3805adddf13c02281a4a1505859b5f6c5397c
zayarlinn <zayarl513@gmail.com>
2025-05-31 15:39:09 +0630

    Remove unnecessary import of os in test_db_connection function

--- Test/test_etl_pipeline.py
+++ Test/test_etl_pipeline.py
@@ -14,7 +14,6 @@ def test_db_connection():
     from sqlalchemy import create_engine, text
     from dotenv import load_dotenv
     load_dotenv()
-    import os
     DATABASE_URL = os.getenv("DATABASE_URL")
     engine = create_engine(DATABASE_URL)
     with engine.connect() as conn:
